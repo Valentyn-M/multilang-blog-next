5d8c0f7e242b2ed33171a65371b75248f3d81d26
Valentyn Mostovyi <mostovoy.v@gmail.com>
2025-07-11 14:56:09 +0200

    style: add styles to PostContent

--- src/components/PostContent/PostContent.tsx
+++ src/components/PostContent/PostContent.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import s from './PostContent.module.scss';
 import { Post } from '@/types/post';
+import Image from 'next/image';
 
 export interface PostContentProps {
   readonly postData: Post;
@@ -11,15 +12,25 @@ export default function PostContent({ postData }: PostContentProps) {
 
   return (
     <section className={clsx(s.postContent, 'container')}>
-      <h1 className={s.title}>{title}</h1>
-      <div className={s.body}>
-        <p>{body}.</p>
-        <p>{body}.</p>
-        <p>{body}.</p>
-        <p>{body}.</p>
-        <p>{body}.</p>
-        <p>{body}.</p>
-        <p>{body}.</p>
+      <div className={s.postWrap}>
+        <h1 className={s.title}>{title}</h1>
+        <Image
+          src="/images/post/image.jpg"
+          alt={title}
+          width={1140}
+          height={885}
+          priority
+          className={s.img}
+        />
+        <div className={s.body}>
+          <p>{body}.</p>
+          <p>{body}.</p>
+          <p>{body}.</p>
+          <p>{body}.</p>
+          <p>{body}.</p>
+          <p>{body}.</p>
+          <p>{body}.</p>
+        </div>
       </div>
     </section>
   );
